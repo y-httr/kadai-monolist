b554c0b0883242782de78a9f3e14479f7bbd47db
hattori <trinity.bb.19@gmail.com>
2017-09-28 08:19:48 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get "signup", to:"users#new"
   resources :users, only: [:show,:create,:new]
   
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
 end
